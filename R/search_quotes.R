9ce6ce49cd2ec428af96742e62447643341a02dc
Phil Chalmers <rphilip.chalmers@gmail.com>
2016-09-03 17:03:36 -0400

    add fuzzy example

--- R/search_quotes.R
+++ R/search_quotes.R
@@ -8,20 +8,19 @@
 #' @return A data frame object containing all quotes that match the search
 #' parameters.
 #' @export
-#' @importFrom stringr str_detect
 #' @seealso \code{\link{statquote}}, \code{\link{quote_topics}}, \code{\link{quotes}}
 #' @examples
 #' search_quotes("^D") # regex to find all quotes that start with "D"
-#' dat <- search_quotes("Tukey")
+#' dat <- search_quotes("Tukey") #all quotes with "Tukey"
 #' dat
+#' search_quotes("bad answer", fuzzy = TRUE) # fuzzy match
 #'
 
-search_quotes <- function(search=NULL, fuzzy=FALSE) {
+search_quotes <- function(search, fuzzy=FALSE) {
   data <- .get.sq()
 
-  if(is.null(search)) {
-    stop("No search parameters entered.")
-  }
+  if(missing(search))
+    stop("No search parameters entered.", call.=FALSE)
 
   merged <- with(data, paste(as.character(text),
                              as.character(topic),
@@ -30,7 +29,7 @@ search_quotes <- function(search=NULL, fuzzy=FALSE) {
   if(fuzzy) OK <- agrep(tolower(search), tolower(merged))
   else OK <- which(str_detect(merged, search))
 
-  if (length(OK)>1) data <- data[OK,]
+  if (length(OK)) data <- data[OK,]
   else stop("The search parameters \'", search, "\' did not match any items.")
 
  return(data)
