2da12a7b4f7ff582bbe65b084f221ceaa3f4d25f
Phil Chalmers <rphilip.chalmers@gmail.com>
2016-09-04 20:35:07 -0400

    fix checks

--- R/quote_cloud.R
+++ R/quote_cloud.R
@@ -4,11 +4,14 @@
 #' a word cloud based upon that filter.
 #'
 #' @param search A character string; used to search the database. Regular
-#' expression characters are allowed. Default is to search all quotes.
+#'   expression characters are allowed. Default is to search all quotes.
 #' @param max.words Logical; designate maximum number of words to be plotted.
 #' @param colors A character vector pertaining to the colors to be used to designate
-#' word frequency. The default is 5 levels, from light to dark green.
+#'   word frequency. The default is 5 levels, from light to dark green.
 #' @return A wordcloud is plotted.
+#' @importFrom tidytext unnest_tokens
+#' @importFrom dplyr tbl_df select anti_join count_
+#' @importFrom wordcloud wordcloud
 #' @export
 #' @seealso \code{\link{statquote}}, \code{\link{quote_topics}}, \code{\link{quotes}}
 #' @examples
@@ -17,24 +20,17 @@
 #' quote_cloud(max.words = 10)
 
 quote_cloud <- function(search = ".*", max.words = 80, colors = NA){
-    library(tidytext)
-    library(dplyr)
-    library(wordcloud)
-
     qt <- search_quotes(search) # defaults to all quotes
+    data("stop_words", package="tidytext", envir = .sq.env)
+    qtidy <- dplyr::tbl_df(qt)
+    qtidy <- qtidy[,!(colnames(qtidy) %in% c("source", "topic", "subtopic"))]
+    qtidy <- tidytext::unnest_tokens(qtidy, "word", "text")
+    qtidy <- dplyr::anti_join(qtidy, .sq.env$stop_words, by = "word")
+    qtidy <- dplyr::count_(qtidy, "word", sort = TRUE)
 
-    data("stop_words", package="tidytext")
-    qtidy <- tbl_df(qt)
-    qtidy <- select(qtidy, -source, -topic, -subtopic)
-    qtidy <- unnest_tokens(qtidy, word, text)
-    qtidy <- anti_join(qtidy, stop_words, by = "word")
-    qtidy <- count(qtidy, word, sort = TRUE)
+    if (is.na(colors))
+      pal <- c("#66C2A4", "#41AE76", "#238B45", "#006D2C", "#00441B")
 
-    if (is.na(colors)){
-      pal <- brewer.pal(9,"BuGn")
-      pal <- pal[-(1:4)]
-    }
-
-    with(qtidy, wordcloud(word, n, max.words = max.words, colors=pal))
+    with(qtidy, wordcloud::wordcloud(word, n, max.words = max.words, colors=pal))
 }
 
