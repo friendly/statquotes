3e86b80c5fe2ac21c88a9d123adee05791db71cf
Phil Chalmers <rphilip.chalmers@gmail.com>
2016-09-04 22:41:00 -0400

    add ... to quote_cloud

--- R/quote_cloud.R
+++ R/quote_cloud.R
@@ -8,19 +8,22 @@
 #' @param max.words Logical; designate maximum number of words to be plotted.
 #' @param colors A character vector pertaining to the colors to be used to designate
 #'   word frequency. The default is 5 levels, from light to dark green.
+#' @param ... additional arguments passed to \code{\link{search_quotes}} and
+#'   \code{\link{wordcloud}}
 #' @return A wordcloud is plotted.
 #' @importFrom tidytext unnest_tokens
 #' @importFrom dplyr tbl_df select anti_join count_
 #' @importFrom wordcloud wordcloud
 #' @export
-#' @seealso \code{\link{statquote}}, \code{\link{quote_topics}}, \code{\link{quotes}}
+#' @seealso \code{\link{statquote}}, \code{\link{quote_topics}}, \code{\link{quotes}},
+#'   \code{\link{search_quotes}}. \code{\link{wordcloud}}
 #' @examples
 #' quote_cloud()
 #' quote_cloud(search = "graph")
 #' quote_cloud(max.words = 10)
 
-quote_cloud <- function(search = ".*", max.words = 80, colors = NA){
-    qt <- search_quotes(search) # defaults to all quotes
+quote_cloud <- function(search = ".*", max.words = 80, colors = NA, ...){
+    qt <- search_quotes(search, ...) # defaults to all quotes
     data("stop_words", package="tidytext", envir = .sq.env)
     qtidy <- dplyr::tbl_df(qt)
     qtidy <- qtidy[,!(colnames(qtidy) %in% c("source", "topic", "subtopic"))]
@@ -31,6 +34,6 @@ quote_cloud <- function(search = ".*", max.words = 80, colors = NA){
     if (is.na(colors))
       pal <- c("#66C2A4", "#41AE76", "#238B45", "#006D2C", "#00441B")
 
-    with(qtidy, wordcloud::wordcloud(word, n, max.words = max.words, colors=pal))
+    with(qtidy, wordcloud::wordcloud(word, n, max.words = max.words, colors=pal, ...))
 }
 
