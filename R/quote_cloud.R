51b53ee84a8343244c455072d6cb825becec591d
Phil Chalmers <rphilip.chalmers@gmail.com>
2016-09-04 23:06:14 -0400

    import all of dplyr for now for better syntax

--- R/quote_cloud.R
+++ R/quote_cloud.R
@@ -11,8 +11,8 @@
 #' @param ... additional arguments passed to \code{\link{search_quotes}} and
 #'   \code{\link{wordcloud}}
 #' @return A wordcloud is plotted.
+#' @import dplyr
 #' @importFrom tidytext unnest_tokens
-#' @importFrom dplyr tbl_df select anti_join count_
 #' @importFrom wordcloud wordcloud
 #' @export
 #' @seealso \code{\link{statquote}}, \code{\link{quote_topics}}, \code{\link{quotes}},
@@ -25,11 +25,10 @@
 quote_cloud <- function(search = ".*", max.words = 80, colors = NA, ...){
     qt <- search_quotes(search, ...) # defaults to all quotes
     data("stop_words", package="tidytext", envir = .sq.env)
-    qtidy <- dplyr::tbl_df(qt)
-    qtidy <- qtidy[,!(colnames(qtidy) %in% c("source", "topic", "subtopic"))]
-    qtidy <- tidytext::unnest_tokens(qtidy, "word", "text")
-    qtidy <- dplyr::anti_join(qtidy, .sq.env$stop_words, by = "word")
-    qtidy <- dplyr::count_(qtidy, "word", sort = TRUE)
+    qtidy <- tbl_df(qt[,!(colnames(qt) %in% c("source", "topic", "subtopic"))])
+    qtidy <- qtidy %>% tidytext::unnest_tokens("word", "text") %>%
+      anti_join(.sq.env$stop_words, by = "word") %>%
+      count_("word", sort = TRUE)
 
     if (is.na(colors))
       pal <- c("#66C2A4", "#41AE76", "#238B45", "#006D2C", "#00441B")
