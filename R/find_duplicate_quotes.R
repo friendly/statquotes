9701f438640cd371e5f93d8bd0e1c097f9ffbcda
Michael Friendly <friendly@yorku.ca>
2019-10-11 09:06:20 -0400

    add find_duplicate_quotes

---
+++ R/find_duplicate_quotes.R
@@ -0,0 +1,33 @@
+
+#' Check for duplicates in master .csv file
+#'
+#' Returns a list with qid, source, and the text where strings are
+#' aggressively fuzzy matched.
+#'
+#' @example
+#' find_duplicate_quotes()
+#'
+find_duplicate_quotes <- function(){
+  dat <- .get.sq()
+  matches <- na.omit(t(sapply(1:nrow(dat), function(i, stringall){
+    string <- stringall[i]
+    tmpstring <- stringall # remove self duplicate
+    tmpstring[i] <- NA
+    ret <- agrep(string, tmpstring, value = FALSE)
+    if(!length(ret)) return(c(NA, NA))
+    c(i, ret)
+  }, stringall = dat$text)))
+
+  if(nrow(matches)){
+    for(i in 1:nrow(matches))
+      matches[matches[i,1L] == matches[,2L], 1L] <- NA
+    matches <- na.omit(matches)
+    ret <- apply(matches, 1L, function(rows){
+      out <- rbind(dat[rows[1], ], dat[rows[2], ])
+      out[,c('qid', 'source', 'text')]
+    })
+    return(ret)
+  }
+  invisible()
+}
+
