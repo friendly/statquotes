f6df20587200558ea1fe0405167953afbf3d6933
philchalmers <rphilip.chalmers@gmail.com>
2023-10-05 16:38:08 -0400

    robustify find_dups() to allow multiple hits

--- R/find_duplicate_quotes.R
+++ R/find_duplicate_quotes.R
@@ -10,28 +10,28 @@
 #' @examples
 #'
 #' # As the number of quotes has grown, this has become very slow.
-#' # find_duplicate_quotes()
+#' # dups <- find_duplicate_quotes()
 #'
 #'
 find_duplicate_quotes <- function(){
   dat <- .get.sq()
-  matches <- na.omit(t(sapply(1:nrow(dat), function(i, stringall){
+  matches <- lapply(1:nrow(dat), function(i, stringall){
     string <- stringall[i]
     tmpstring <- stringall # remove self duplicate
     tmpstring[i] <- NA
     ret <- agrep(string, tmpstring, value = FALSE)
-    if(!length(ret)) return(c(NA, NA))
-    c(i, ret)
-  }, stringall = dat$text)))
+    ret
+  }, stringall = dat$text)
+  whc <- which(sapply(matches, function(x) length(x) > 0L))
+  matches <- cbind(whc, t(do.call(cbind, matches[whc])))
 
   if(nrow(matches)){
-    for(i in 1:nrow(matches))
-      matches[matches[i,1L] == matches[,2L], 1L] <- NA
-    matches <- na.omit(matches)
     ret <- apply(matches, 1L, function(rows){
-      out <- rbind(dat[rows[1], ], dat[rows[2], ])
-      out[,c('qid', 'source', 'text')]
+      rows <- unique(rows)
+      out <- dat[rows, ]
+      out
     })
+    names(ret) <- paste0('DUPLICATE_', 1L:length(ret))
     return(ret)
   }
   invisible()
