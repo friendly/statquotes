3328cb396b625b0c26f6191f4d17aa630984ed1b
Michael Friendly <friendly@yorku.ca>
2023-10-08 14:17:18 -0400

    edit quote-stats.R

--- data-raw/quote-stats.R
+++ data-raw/quote-stats.R
@@ -9,22 +9,42 @@ library(dplyr)
 qt <- get_quotes()
 text <- qt$text
 
-# count characters, words, sentences
+#' ## count characters, words, sentences
 stats <- data.frame(
   qid = qt$qid,
-  chars = stringr::str_count(text, boundary("character")),
-  words = stringr::str_count(text, boundary("word")),
+  chars = str_count(text, boundary("character")),
+  words = str_count(text, boundary("word")),
   sent = str_count(text, boundary("sentence")),
   txt = substr(qt$text, 1, 40)
 )
 
 summary(stats)
 
+#' ### histograms
 hist(stats$chars)
 hist(stats$words)
 hist(stats$sent)
 
+#' ### which are the longest?
 stats |>
-  slice_max(words, n=10) |>
-  arrange(qid)
+  dplyr::slice_max(words, n=12) |>
+  dplyr::arrange(qid)
+
+
+#' ## examine tag distribution
+tags <- qt$tags
+terms <- tags |> str_split(pattern = ",\\s*") |> unlist()
+
+table(terms)
+
+#' ## top / bottom tag terms
+
+#' should use quote_tags(), but it doesn't allow a descending option
+
+terms |> table() |> sort() |> as.data.frame() |> head(10)
+
+terms |> table() |> sort(decreasing=TRUE) |> as.data.frame() |> head(10)
+
+
+
 
